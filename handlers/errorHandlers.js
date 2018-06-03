4d809b4f486fdde29b14a87b896b21d0b68f8e37
Brett Commandeur <bcmdr.co@gmail.com>
2018-06-02 19:14:08 -0600

    remove debugging

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -44,6 +44,7 @@ exports.flashValidationErrors = (err, req, res, next) => {
 */
 
 exports.flashLinkErrors = (err, req, res, next) => {
+
   if (!err.code) return next(err)
   // link not found errors look like
   if(err.code !== 'ENOTFOUND') {
